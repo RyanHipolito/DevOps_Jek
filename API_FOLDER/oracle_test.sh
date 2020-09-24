0d91c983184f12dd44e7c4fe98d00a4e18815482
RyanHipolito <67422622+RyanHipolito@users.noreply.github.com>
2020-09-24 15:07:33 +0800

    Update oracle_test.sh

--- API_FOLDER/oracle_test.sh
+++ API_FOLDER/oracle_test.sh
@@ -1,4 +1,4 @@
-. /root/.bash_profile
+. /home/ec2-user/.bash_profile
 SQL_statment=`cat $1`
 
 sqlplus "C##Devops/devops12@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.31.4.53)(PORT=1521))(CONNECT_DATA=(SID=cdb1)))" <<END
