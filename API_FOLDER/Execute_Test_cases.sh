86045d26a44330a27f8216f9a8150191c234f3f6
RyanHipolito <67422622+RyanHipolito@users.noreply.github.com>
2020-11-02 20:46:52 +0800

    Update Execute_Test_cases.sh

--- API_FOLDER/Execute_Test_cases.sh
+++ API_FOLDER/Execute_Test_cases.sh
@@ -2,7 +2,7 @@
 . /home/ec2-user/.bash_profile
 SQL_statment=`cat $1`
 
-SPOOL_FILE=${WORKSPACE}.log
+SPOOL_FILE=${WORKSPACE}/execute.log
 
 SQLPLUS_OUTPUT=`sqlplus "C##Devops/devops12@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.31.4.53)(PORT=1521))(CONNECT_DATA=(SID=cdb1)))" << EOF
 		SET HEAD OFF
@@ -21,7 +21,8 @@ RC=$?
 if [[ $RC != 0 ]] ; then
 
     echo " RDBMS exit code : $RC  "     | tee -a ${LOG_FILE}
-    cat ${SPOOL_FILE}                 
+    cat ${SPOOL_FILE}         
+    cat $SQLPLUS_OUTPUT > ${WORKSPACE}/execute_test_cases.logs
     exit 3
 
 fi
