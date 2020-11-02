22282b32e572919e5c0f43fdc02c182466b1782a
RyanHipolito <67422622+RyanHipolito@users.noreply.github.com>
2020-11-02 22:07:36 +0800

    Update Test_cases.sql

--- API_FOLDER/Test_cases.sql
+++ API_FOLDER/Test_cases.sql
@@ -4,7 +4,7 @@ PD.first_name||' '||PD.last_name AS FULL_NAME,
 ID.Status,
 ID.Address1||' '||ID.Address2 AS FULL_ADDRESS,
 ID.Telephone,
-ID.Mobile
+ID.Mobile_Number
 from Personal_Details pd INNER JOIN Information_Details ID
 ON PD.PERSONAL_ID = ID.PERSONAL_ID
 WHERE PD.PERSONAL_ID ='12345';
