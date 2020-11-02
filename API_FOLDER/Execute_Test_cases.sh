be979f8d3b41440fd8565dd4319ca44cba63a551
RyanHipolito <67422622+RyanHipolito@users.noreply.github.com>
2020-11-02 23:22:08 +0800

    Update Execute_Test_cases.sh

--- API_FOLDER/Execute_Test_cases.sh
+++ API_FOLDER/Execute_Test_cases.sh
@@ -2,7 +2,7 @@
 . /home/ec2-user/.bash_profile
 SQL_statment=`cat $1`
 
-SPOOL_FILE=/var/lib/jenkins/workspace/Oracle_Pipeline/API_FOLDER/execute.log
+SPOOL_FILE=/var/lib/jenkins/workspace/Oracle_Pipeline/execute.log
 
 SQLPLUS_OUTPUT=`sqlplus "C##Devops/devops12@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.31.4.53)(PORT=1521))(CONNECT_DATA=(SID=cdb1)))" << EOF
                 SET HEAD OFF
@@ -21,9 +21,9 @@ RC=$?
 if [[ $RC != 0 ]] ; then
         echo " RDBMS exit code : $RC  "     | tee -a ${LOG_FILE}
 else
-echo "All Test Scenarios are Completed" > /var/lib/jenkins/workspace/Oracle_Pipeline/API_FOLDER/execute_test.log
-echo "#####################################################" >> /var/lib/jenkins/workspace/Oracle_Pipeline/API_FOLDER/execute_test.log
-echo "## All Queries are Passed and Ready for Deployment ##" >> /var/lib/jenkins/workspace/Oracle_Pipeline/API_FOLDER/execute_test.log
-echo "#####################################################" >> /var/lib/jenkins/workspace/Oracle_Pipeline/API_FOLDER/execute_test.log
-echo ${SQLPLUS_OUTPUT} >> /var/lib/jenkins/workspace/Oracle_Pipeline/API_FOLDER/execute_test.log
+echo "All Test Scenarios are Completed" > /var/lib/jenkins/workspace/Oracle_Pipeline/execute_test.log
+echo "#####################################################" >> /var/lib/jenkins/workspace/Oracle_Pipeline/execute_test.log
+echo "## All Queries are Passed and Ready for Deployment ##" >> /var/lib/jenkins/workspace/Oracle_Pipeline/execute_test.log
+echo "#####################################################" >> /var/lib/jenkins/workspace/Oracle_Pipeline/execute_test.log
+echo ${SQLPLUS_OUTPUT} >> /var/lib/jenkins/workspace/Oracle_Pipeline/execute_test.log
 fi
